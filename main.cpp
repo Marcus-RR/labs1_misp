3fb710ac2b2283135ad8a34172bdef25ee391af0
Marcus-RR <145934289+Marcus-RR@users.noreply.github.com>
2023-12-13 22:59:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,41 @@
+#include <iostream>
 #include "byblik.h"
-using namespace std;
-bool isValid(const string& s)
-{
-    for(auto c:s)
-        if (!isalpha(c) || !isupper(c))
-            return false;
-    return true;
-}
 
 int main()
 {
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    bool isTrue = true;
-    wcout << L"Добро пожаловать в программу для шифрования." << endl;
+    std::string plaintext;
     int key;
-    wstring text;
-    wstring action;
-    wcout<<L"Введите ключ: ";
-    wcin>>key;
-    if (!wcin.good()) {
-        wcerr<<L"Ключ неверный!(ключ должен быть числовым значением)\n";
-        return 1;
-    }
+    unsigned op;
 
-    ByB cipher(key);
+    std::cout << "Enter number of columns: ";
+    std::cin >> key;
+    std::cin.ignore();
+    byblik cipher(key);
+
     do {
-        wcout<<L"Укажите действие (exit-выход, 1-шифрование, 2-расшифрование): ";
-        wcin>>action;
-        if (action != L"1" && action != L"2" && action != L"exit") {
-            wcout << L"Введите любую последовательность символов, чтобы продолжить.\n";
-            wcin >> action;
-        }
+        std::cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        std::cin>>op;
+        if (op > 2) {
+            std::cout<<"Illegal operation\n";
+        } else if (op >0) {
 
-        if (action == L"1") {
-            wcout << L"Введите строку:"<<endl;
-            wcin>> text;
-            wcout << L"Зашифрованная строка:"<<cipher.encrypt(text)<<endl;
-        }
-        if (action == L"2") {
-            wcout << L"Введите строку:"<<endl;
-            wcin>> text;
-            wcout << L"Расшифрованная строка:"<<cipher.decrypt(text)<<endl;
-        }
-        if (action == L"exit") {
-            wcout << L"Программа завершила работу." << endl;
-            isTrue = false;
-            break;
-        }
+            std::cout << "Enter plaintext: ";
+            std::cin.ignore();
+            std::getline(std::cin, plaintext);
 
+            if (op==1) {
 
-    } while (isTrue != false);
+                std::string ciphertext = cipher.encrypt(plaintext);
+                std::cout << "Encrypted text: " << ciphertext << std::endl;
+
+            } else {
+
+                std::string decryptedText = cipher.decrypt(plaintext);
+                std::cout << "Decrypted text: " << decryptedText << std::endl;
+
+            }
+
+        }
+    } while (op!=0);
     return 0;
 }
